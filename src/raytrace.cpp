b517bf36d0ea5b97740dbf9495de493be7c9fe88
Shubham Nayak <shubhamnayak392reborn@gmail.com>
2026-08-16 20:39:56 +0530

    cool stuff create image.ppm

--- src/raytrace.cpp
+++ src/raytrace.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include <print>
+#include "Color.hpp"
 
 int main() {
 	constexpr int image_width { 256 };
@@ -7,16 +9,15 @@ int main() {
 	std::println("P3\n{} {} \n255\n", image_width, image_height);
 
 	for (int j{0}; j < image_height; ++j) {
+		std::println(std::cerr, "\rScanlines remaining: {} ", (image_height - j));
 		for (int i{0}; i < image_width; ++i) {
-			auto r = static_cast<double>(i) / (image_width - 1);
-			auto g = static_cast<double>(j) / (image_height - 1);
-			auto b = 0.0;
+			auto pixel_color = Color<double>(
+				static_cast<double>(i)/(image_width-1), 
+				static_cast<double>(j)/(image_height-1),
+				0.0);
 
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			std::println("{} {} {}", ir, ig, ib);
+			writeColor(std::cout, pixel_color);
 		}
 	}
+	std::println(std::cerr, "\rDone.              ");
 }
